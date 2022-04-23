c7b3535408ac70739770b827c4d6117e9291acb4
Yash-RajSingh <yashraj45202@gmail.com>
2022-04-24 01:25:22 +0530

    Initial commit

---
+++ scripts/display.js
@@ -0,0 +1,56 @@
+const sidebar = document.getElementById('sidebar');
+const sideContainer = document.getElementById('sidebar-container');
+const statContainer = document.getElementById('stats-container');
+const username = sessionStorage.getItem('uname');
+var flag= 0;
+
+const expand = () => {
+    if(flag==0){
+        flag=1;
+        sidebar.style.width = '15%';
+        sidebar.classList.add("animate__slideInLeft");
+        const myTimeout = setTimeout(remove1, 3000);
+
+        function remove1() {
+            sidebar.classList.remove("animate__slideInLeft");
+        }
+    }
+    else if(flag==1){
+        sidebar.style.width = '2.1%';
+        flag=0;
+    }
+}
+
+$('#menu-icon').click(function(){
+    $('#sidebar-container').toggle();
+    expand()
+})
+
+
+$('#sidebar button').click(function(){
+    console.log("click");
+    window.location.href = "./login.html";
+})
+
+const usercheck = () =>{
+    const profile = document.getElementById('profile')
+    if(username != null){
+        profile.innerHTML = `<img src="../assets/icons/about.png" class="siderbar-icon"><h3 onclick="openProfile()">${username}</h3>
+        <br><p id="logout" onclick="logout()">Logout</p>
+        `;
+    }
+    else {
+        console.log("not logged in");
+        return 0;
+    }
+}
+usercheck();
+
+const openProfile = () =>{
+    window.location.href = "../profile.html";
+}
+
+const logout = () =>{
+    sessionStorage.clear();
+    window.location.href = "../index.html"
+}
